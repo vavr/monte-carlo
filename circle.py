adcfaaec419c305d829b5d668268bc4ddf8d78e0
Igor Vavrzhin <igor.vavrjin@gmail.com>
2022-02-09 14:17:59 +0700

    it works

--- circle.py
+++ circle.py
@@ -1,116 +1,126 @@
 from tkinter import *
-from typing import List, Any
 
-from R2Graph import *
+import random
 
+need_stop = False
+R = 300
+x_center = 100 + R
+y_center = 100 + R
+N = 1000000
 
 def main():
     rootWindow = Tk()
-    rootWindow.title("Triangle")
-    rootWindow.geometry("800x600")
-
-    points = []
+    rootWindow.title("PI via monte carlo method")
+    rootWindow.geometry("800x800")
 
     panel = Frame(rootWindow)
     drawArea = Canvas(rootWindow, bg="white")
 
+    def set_center():
+        global x_center, y_center
+        x_center = 100 + R
+        y_center = 100 + R
+
+    def startupPosition():
+        global need_stop
+        need_stop = False
+
+        set_center()
+        drawArea.create_rectangle(100, 100, R * 2 + 100, R * 2 + 100, fill="", outline="black", width=2)
+        drawArea.create_oval(100, 100, R * 2 + 100, R * 2 + 100, fill="", outline="black", width=2)
+
+    def in_circle(x, y):
+        return (x - x_center) ** 2 + (y - y_center) ** 2 <= R ** 2
+
+    def drawPoint(x, y):
+
+        if in_circle(x, y):
+            color = "green"
+        else:
+            color = "red"
+
+        drawArea.create_oval(x-1, y-1, x+1, y+1, fill=color, outline=color)
+        drawArea.update()
+
+    def start():
+        clear()
+        global need_stop, N
+        need_result = 0
+        for i in range(N):
+            if need_stop:
+                return
+            x = random.uniform(100, R * 2 + 100)
+            y = random.uniform(100, R * 2 + 100)
+            if in_circle(x, y):
+                need_result += 1
+            drawPoint(x, y)
+            if i % 1000 == 0:
+                piValue['text'] = (need_result / (i + 1)) * 4
+            iterValue['text'] = i
+
+    def stop():
+        global need_stop
+        need_stop = True
+
     def clear():
         drawArea.delete("all")
-        points.clear()
+        startupPosition()
+        iterValue['text'] = 0
+        piValue['text'] = 0
 
+    def change_radius(e):
+        global R, need_stop
+        R = int(e.widget.get())
+        need_stop = True
+        clear()
+
+    def change_n(e):
+        global N, need_stop
+        N = int(e.widget.get())
+        need_stop = True
+        clear()
+
+    startButton = Button(panel, text="Start", command=start)
+    stopButton = Button(panel, text="Stop", command=stop)
     clearButton = Button(panel, text="Clear", command=clear)
 
-    def drawTriangle():
-        if len(points) >= 3:
-            # drawArea.delete("all")
-            drawArea.create_line(
-                points[0].x, points[0].y,
-                points[1].x, points[1].y,
-                points[2].x, points[2].y,
-                points[0].x, points[0].y,
-                fill="blue", width=2
-            )
-            (center, radius, bisectrixes) = inCircle(points)
-            # Draw an inscribed circle
-            drawArea.create_oval(
-                center.x - radius, center.y - radius,
-                center.x + radius, center.y + radius,
-                fill="", outline="red", width=2
-            )
+    piValueText = Label(panel, text="Текущее приближение PI: ")
+    piValue = Label(panel, text="0")
 
-            # Draw bisectrixes
-            for i in range(3):
-                drawArea.create_line(
-                    points[i].x, points[i].y,
-                    bisectrixes[i].x, bisectrixes[i].y,
-                    fill="darkGreen", width=1
-                )
-            # Draw the center of circle
-            drawArea.create_oval(
-                center.x - 4, center.y - 4,
-                center.x + 4, center.y + 4,
-                fill="red", outline="red"
-            )
+    iterValueText = Label(panel, text="Кол-во итераций: ")
+    iterValue = Label(panel, text="0")
 
-    drawButton = Button(panel, text="Draw", command=drawTriangle)
+    piValue.pack(side=RIGHT)
+    piValueText.pack(side=RIGHT)
+
+    iterValue.pack(side=RIGHT)
+    iterValueText.pack(side=RIGHT)
 
     panel.pack(fill=X, padx=4, pady=4)
     clearButton.pack(side=LEFT)
-    drawButton.pack(side=LEFT)
+    startButton.pack(side=LEFT)
+    stopButton.pack(side=LEFT)
+
+    radiusValue = Entry(panel, width=5)
+    radiusValue.bind("<Return>", change_radius)
+    radiusLabel = Label(panel, text="Радиус: ")
+    radiusLabel.pack(side=LEFT)
+    radiusValue.pack(side=LEFT)
+
+    nValue = Entry(panel, width=10)
+    nValue.bind("<Return>", change_n)
+    nLabel = Label(panel, text="К-во итераций: ")
+    nLabel.pack(side=LEFT)
+    nValue.pack(side=LEFT)
+
     drawArea.pack(fill=BOTH, side=TOP, expand=True, padx=4, pady=4)
 
-    def click(e):
-        if len(points) >= 3:
-            clear()
-        points.append(R2Point(e.x, e.y))
-        drawArea.create_line(
-            e.x - 8, e.y, e.x + 8, e.y, fill="red", width=3
-        )
-        drawArea.create_line(
-            e.x, e.y - 8, e.x, e.y + 8, fill="red", width=3
-        )
+    radiusValue.insert(0, R)
+    nValue.insert(1000, N)
 
-    drawArea.bind("<Button-1>", click)
+    startupPosition()
 
     rootWindow.mainloop()
-
-
-# Compute bisectrixes of triangle and
-# a center & radius of inscribed circle
-def inCircle(vertices):
-    bisectrixes = []
-    for i in range(3):
-        iPrev = i - 1
-        if iPrev < 0:
-            iPrep = 2
-        iNext = i + 1
-        if iNext >= 3:
-            iNext = 0
-        vPrev = (vertices[iPrev] - vertices[i]).normalized()
-        vNext = (vertices[iNext] - vertices[i]).normalized()
-        b = vPrev + vNext  # The direction vector of i-th bisectrix
-
-        # The point of bisectrix is calculated as intersection of 2 lines,
-        # where a line is defined by a point and a direction vector
-        (_, bisectrix) = intersectLines(
-            vertices[i], b,  # line of bisectrix
-            vertices[iPrev],  # side of triangle
-            vertices[iNext] - vertices[iPrev]
-        )
-        bisectrixes.append(bisectrix)
-
-    # A circle center is an intersection of 2 bisectrixes
-    (_, center) = intersectLines(
-        vertices[0], bisectrixes[0] - vertices[0],
-        vertices[1], bisectrixes[1] - vertices[1]
-    )
-    # The circle radius equals to the distance from the center
-    # to the first side of triangle
-    radius = center.distanceToLine(
-        vertices[0], vertices[1] - vertices[0]
-    )
-    return (center, radius, bisectrixes)
-
 
 if __name__ == "__main__":
     main()
